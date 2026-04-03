52599bf23b45c4970126da4d683e4c961fecf006
mbmmle <maciekbart55@gmail.com>
2026-04-03 22:17:37 +0200

    Fully added Bronze Layer

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM jupyter/pyspark-notebook:latest
 
 COPY requirements.txt /tmp/requirements.txt
+COPY work /home/jovyan/work
 
-RUN pip install --no-cache-dir -r /tmp/requirements.txt
+RUN pip install --no-cache-dir -r /tmp/requirements.txt \
+	&& pip install --no-cache-dir -e /home/jovyan/work
---
+++ work/config/__init__.py
@@ -0,0 +1,18 @@
+jdbc_url = "jdbc:postgresql://postgres:5432/Warsaw_Bus_DB"
+
+db_properties = {
+    "user": "admin",
+    "password": "admin",
+    "driver": "org.postgresql.Driver",
+    "rewriteBatchedInserts": "true",
+    "batchsize": "50000",
+    "numPartitions": "16",
+}
+
+DB_CONFIG = {
+    "dbname": "Warsaw_Bus_DB",
+    "user": "admin",
+    "password": "admin",
+    "host": "host.docker.internal",
+    "port": "5432",
+}
